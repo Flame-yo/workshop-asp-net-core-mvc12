3d31bb8d48a3cef2aa27690a3838892770e8b1fa
Gustavo Henrique <gustavohffs@gmail.com>
2024-03-13 00:27:08 -0300

    Project Create

---
+++ SalesWebMvc12/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc12.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc12.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc12/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc12.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
